6fe8a8bbc7382ad0f87b1b56db26f3d170c45149
Paul Amani <67480521+pacis32@users.noreply.github.com>
2022-11-15 21:50:52 -0600

    adding Lab8

---
+++ Lab8/index.js
@@ -0,0 +1,35 @@
+
+window.onload= function (){
+  getInfo();
+  document.getElementById('refresh').onclick=getInfo;
+}
+async function getInfo(){
+  const response= await fetch('https://randomuser.me/api/?results=5');
+  const dataList= await response.json();
+   displayEmp(dataList.results);
+   
+   }
+
+   function displayEmp(data) {
+    const empDiv = document.getElementById('employees');
+    empDiv.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        let emp = data[i];
+        let template = `
+        <div class="col-2">
+            <img src="${emp.picture.large}" />
+        </div>
+        <div class="col-2 text-end">
+            <p class="fw-bold">${emp.name.first + " " + emp.name.last}</p>
+            <p>Gender: ${emp.gender}</p>
+            <p>${emp.email}</p>
+        </div>
+        <hr>
+        `
+        let div = document.createElement('div');
+        div.innerHTML = template; 
+        div.classList = 'row'; 
+        empDiv.appendChild(div);
+    }
+}
+
